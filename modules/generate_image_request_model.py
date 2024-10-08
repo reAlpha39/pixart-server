f314cbbcd3fef4498d8923067454edfb881b2214
reAlpha39 <yunusw718@gmail.com>
2024-10-08 09:00:36 +0700

    improve: remove Optional type

--- modules/generate_image_request_model.py
+++ modules/generate_image_request_model.py
@@ -3,15 +3,15 @@ from typing import Optional
 
 class ImageRequest(BaseModel):
     prompt: str
-    model: Optional[str] = "PixArt-Sigma-900M"
-    negative_prompt: Optional[str] = "low quality, bad, watermark, human, people, person"
-    seed: Optional[int] = 42
-    randomize_seed: Optional[bool] = False
-    width: Optional[int] = 1024
-    height: Optional[int] = 1024
-    guidance_scale: Optional[float] = 3.5
-    num_inference_steps: Optional[int] = 28
-    generate_prompt: Optional[bool] = False,
-    prompt_model: Optional[str] = "gemma2:latest",
-    keep_alive_prompt_model: Optional[int] = 0,
-    use_resolution_binning: Optional[bool] = True,
+    model: str = "PixArt-Sigma-900M"
+    negative_prompt: str = "low quality, bad, watermark, human, people, person"
+    seed: int = 42
+    randomize_seed: bool = False
+    width: int = 1024
+    height: int = 1024
+    guidance_scale: float = 3.5
+    num_inference_steps: int = 28
+    generate_prompt: bool = False
+    prompt_model: str = "gemma2:latest"
+    keep_alive_prompt_model: int = 0
+    use_resolution_binning: bool = True
